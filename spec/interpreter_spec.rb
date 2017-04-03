580427c0a1dd717856bfea6332ccc06c8f35fc26
bejczib <bejczi.balint@gmail.com>
2017-04-03 23:00:18 +0200

    test: can handle *,/

--- spec/interpreter_spec.rb
+++ spec/interpreter_spec.rb
@@ -6,7 +6,9 @@ describe Interpreter do
   {expression: "1+1",expected: 2},
   {expression: "1-1",expected: 0},
   {expression: "12+1",expected: 13},
-  {expression: "4+16",expected: 20}
+  {expression: "4+16",expected: 20},
+  {expression: "4*2",expected: 8},
+  {expression: "4/2",expected: 2}
 ].each do |test_case|
     context "when the the expression to eval is #{test_case[:expression]}" do
 
